58057627c1abb0fc68c66458cafed73e1e2ee24f
Osetrov E <rabota87890012@gmail.com>
2022-07-01 18:20:00 +0300

    demo

--- tasks_management/health_check/views.py
+++ tasks_management/health_check/views.py
@@ -1,3 +1,76 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+users = [
+    {
+        "id": 1,
+        "email": "anton.osetrov@gmail.com",
+        "age": 22,
+        "gender": "men",
+        "profession": "IT developer",
+    },
+    {
+        "id": 2,
+        "email": "qwe.132@gmail.com",
+        "age": 25,
+        "gender": "woman",
+        "profession": "nurse",
+    },
+    {
+        "id": 3,
+        "email": "sdfsdf@gmail.com",
+        "age": 40,
+        "gender": "men",
+        "profession": "driver",
+    },
+    {
+        "id": 4,
+        "email": "123.qeasd23@gmail.com",
+        "age": 32,
+        "gender": "woman",
+        "profession": "accountant",
+    },
+    {
+        "id": 5,
+        "email": "13qweer.qwe@gmail.com",
+        "age": 17,
+        "gender": "woman",
+        "profession": "business woman",
+    },
+    {
+        "id": 6,
+        "email": "1456123@gmail.com",
+        "age": 36,
+        "gender": "men",
+        "profession": "salesman",
+    },
+    {
+        "id": 7,
+        "email": "qweqweqwe.576@gmail.com",
+        "age": 29,
+        "gender": "woman",
+        "profession": "unemployed",
+    },
+]
+
+class ReqGet(APIView):
+    def get(self, request, format=None):
+        return Response({'req type': 'Get'})
+
+    def post(self, request):
+        return Response({'req type': 'Post'})
+
+
+class UsersList(APIView):
+    def get(self, request):
+        def analyze_object(users):
+            count = 0
+            if isinstance(object, dict):
+                for items in object:
+                    count += analyze_object(object[items])
+
+        return Response(request)
+
+for i in users:
+    if i['age'] == query:
--- tasks_management/tasks_management/urls.py
+++ tasks_management/tasks_management/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from health_check.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/health', ReqGet.as_view()),
+    path('api/v1/users', UsersList.as_view()),
 ]
